81717e63f5c21379395406cf0d5361abc0411850
Vixndotcom <vixndotcom@gmail.com>
2024-03-08 04:50:23 -1200

    fifth

--- src/components/about/ImagesP.jsx
+++ src/components/about/ImagesP.jsx
@@ -12,7 +12,11 @@ const ImagesP = () => {
     </Background>
 
     <div className='flex md:w-[370px] sm:w-[370px] ss:w-[470px] mt-10 text-center p-6 textt'>
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur consectetur repellat, cumque, unde fugit atque error veniam distinctio doloribus tempora hic, voluptatum dolorem laborum. Earum vero placeat, quaerat sapiente ut minus praesentium odio ad eaque nostrum accusamus quo architecto quae voluptate temporibus dolore doloremque, quas itaque sunt voluptas esse eligendi amet nihil mollitia. Repellendus perspiciatis illo consequuntur eos vero qui quos nostrum doloribus. Tenetur recusandae exercitationem doloribus consequuntur, commodi perspiciatis dolorum molestias sed, libero hic similique facere voluptatum id est?
+       Earum vero placeat, quaerat sapiente ut minus praesentium odio ad eaque nostrum accusamus 
+       quo architecto quae voluptate temporibus dolore doloremque, quas itaque sunt voluptas esse
+        eligendi amet nihil mollitia. Repellendus perspiciatis illo consequuntur eos vero qui quos
+         nostrum doloribus. Tenetur recusandae exercitationem doloribus consequuntur, commodi perspiciatis
+          dolorum molestias sed, libero hic similique facere voluptatum id est?
 
 
  
