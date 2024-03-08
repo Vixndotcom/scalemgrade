39d0605e6f93ddda4b453c5ca2f9fba73c9d924c
Vixndotcom <vixndotcom@gmail.com>
2024-03-08 04:12:10 -1200

    second

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -9,7 +9,7 @@ import { Link } from 'react-router-dom'
 
 const Hero = () => {
    return (
-     <div className='content z-[99] md:mt-20 bg-paper' >
+     <div className='content z-[99] md:mt-20 sm:mt-10 bg-paper' >
        
       <Dynamics>
        <h1 className='content-inner-bold shadow-text md:mt-10 ' >
