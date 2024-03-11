46f690caced2026737bbf068fd99b331a89429f3
Vixndotcom <vixndotcom@gmail.com>
2024-03-11 03:04:12 -1200

    about-images

--- src/components/about/Cardd.jsx
+++ src/components/about/Cardd.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from '../../style'
 import DynamicLeft from '../Dynamics'
-import { lamp, success } from '../../assets'
+import { desk, lamp, success } from '../../assets'
 
 const Cardd = () => {
   return (
@@ -9,8 +9,8 @@ const Cardd = () => {
     <section className={`${styles.flexCenter} mb-20 flex flex-col mt-10 `}>
 
       <DynamicLeft>
-          <div className=' text-center about-card   p-2  about-morph rounded-t-[24px] rounded-b-[8px] rounded-b-[12px] h-[160px]
-         border-[1px] border-yellow-500'>
+          <div className=' text-center about-card   p-2  about-morph rounded-t-[20px] rounded-b-[8px] rounded-b-[12px] h-[160px]
+         border-[1px] border-yellow-500 mx-auto'>
           
            <h1 className='text-[32px] font-semibold p-4 shadow-text text-white mx-auto '>
              Your Academics in Safe Hands
@@ -18,15 +18,15 @@ const Cardd = () => {
          
         </div> 
         </DynamicLeft>
-
-        <div className='flex flex-col'>
-
+        
+         <div className='flex flex-col'>
+        
           <div className='about-img '>
-            <img src={lamp} alt="" className='w-[60%] mx-auto rounded-[8px]  '/>
+            <img src={desk} alt="" className='w-[70%] mx-auto rounded-[8px]  '/>
           </div>
       
       
-        </div>
+        </div> 
         
     </section>
     
--- src/components/about/ImagesP.jsx
+++ src/components/about/ImagesP.jsx
@@ -1,31 +1,38 @@
-import React from 'react'
-import DynamicLeft from '../DynamicLeft'
-import { carddd, desk, lamp, student } from '../../assets'
-import Cardd from './Cardd'
-import { Background, Parallax } from 'react-parallax'
+import React from 'react';
+import { boy, boyy, desk, girl, lamp, library, student } from '../../assets';
+import { Parallax } from 'react-parallax';
+import Cardd from './Cardd';
 
-function ImagesP () {
+
+const ImagesP = () => {
+    const containerStyle = {
+        height: '33.3vh',
+        width: '100vw',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginRight: 'auto',
+        marginLeft: 'auto',
+
+    };
+
+    const imageStyle = {
+       width: '60%',
+       height: 'auto',
+       maxHeight: '20%',
+    };
+
+
   return (
+    <div style={containerStyle}>
+        <Parallax bgImage={lamp} strength={600}>
+            <div style={containerStyle}>
+                  {/* < alt=""  style={imageStyle}/>   */}
+               {/* tolani */}
+            </div>
 
-      
-    //  <img src={desk} alt="" className='w-[96vw] mx-auto rounded-b-[8px]' />
-    
-         
-    <Parallax strength={600} height={800} >
-    <Background  className='mt-20 cell rounded-b-[8px]' style={{width:'96vw'}}  >
-      
-  <img src={desk}   />
-    </Background>
-
-    <div className='flex md:w-[240px] sm:w-[240px] ss:w-[480px] md:mt-10 text-center align-items-center p-2 textt  '>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos veritatis numquam hic, esse itaque nulla cupiditate
-     qui, debitis libero ratione atque cumque deserunt ullam. Cupiditate voluptate exercitationem ad aperiam esse et
-      quia autem similique vel labore quae dicta suscipit sint, quas ab. Quod asperiores illo eveniet, fugiat explicabo 
-      fugit soluta quis natus recusandae 
-  </div>
-</Parallax>
-
-
+        </Parallax>
+    </div>
   )
 }
 
