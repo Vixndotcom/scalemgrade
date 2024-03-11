d745bcdc7c21e8b2a715c04330593eb966a7fb8c
Vixndotcom <vixndotcom@gmail.com>
2024-03-11 03:26:23 -1200

    approach

--- src/components/about/ParallaxText.jsx
+++ src/components/about/ParallaxText.jsx
@@ -22,7 +22,7 @@ const handleScroll = () => {
 
  return(
     <div ref={parallaxRef} className='parallax-contain'>
-        <div className='para-textt' style={{ transform: `translateY(-${scrollPosition * 0.1}px)` }}>
+        <div className='para-textt' style={{ transform: `translateY(-${scrollPosition * 0.2}px)` }}>
     
            <p className='text-white w-[312px] text-center   text-[20px] leading-[40px] '>
            <span className='text-[60px] text-yellow-500  '>"</span><br /> With a commitment to excellence, we offer a supportive environment where challenges are met with expertise, 
