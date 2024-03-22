aa013856af878a5d80f19012cd16cc89251d9e3b
Vixndotcom <vixndotcom@gmail.com>
2024-03-22 04:11:48 -1200

    height

--- src/components/services/Loop.jsx
+++ src/components/services/Loop.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loop = () => {
   return (
-    <div className='text-[28px]  font-semibold text-white scroll-text'>
+    <div className='text-[28px]  font-semibold text-white scroll-text mt-6 mb-6'>
         <p>
             Let's make things Easier
         </p>
